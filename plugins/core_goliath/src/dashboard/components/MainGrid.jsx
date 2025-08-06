5ae9577569bbd192aad426271a6f16e1c08ec9dc
Niurouxing <z754346443@gmail.com>
2025-08-06 19:13:35 +0800

    remove copyright

--- plugins/core_goliath/src/dashboard/components/MainGrid.jsx
+++ plugins/core_goliath/src/dashboard/components/MainGrid.jsx
@@ -3,7 +3,6 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import Copyright from '../internals/components/Copyright';
 
 
 const data = [
@@ -81,7 +80,6 @@ export default function MainGrid() {
           </Stack>
         </Grid>
       </Grid>
-      <Copyright sx={{ my: 4 }} />
     </Box>
   );
 }
